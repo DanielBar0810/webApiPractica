b99762980d644e0d822520c971abcc6dd10b01bf
Daniel <daniel.barrera@catolica.edu.sv>
2023-03-24 22:49:28 -0600

    .

--- webApiPractica/Controllers/equiposController.cs
+++ webApiPractica/Controllers/equiposController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using webApiPractica.Models;
 using Microsoft.EntityFrameworkCore;
+using webApiPractica.Models;
 
 namespace webApiPractica.Controllers
 {
-    [Route("equipos")]
+    [Route("api/[controller]")]
     [ApiController]
     public class equiposController : ControllerBase
     {
@@ -15,120 +15,118 @@ namespace webApiPractica.Controllers
         {
             _equiposContexto = equiposContexto;
         }
-
         [HttpGet]
         [Route("GetAll")]
         public IActionResult Get()
         {
-            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos select e).ToList();
-
+            var listadoEquipo = (from e in _equiposContexto.equipos
+                                 join m in _equiposContexto.marcas on e.marca_id equals m.id_marcas
+                                 join te in _equiposContexto.tipo_equipo on e.tipo_equipo_id equals te.id_tipo_equipo
+                                 select new
+                                 {
+                                     e.id_equipos,
+                                     e.nombre,
+                                     e.descripcion,
+                                     e.tipo_equipo_id,
+                                     tipo_descripcion = te.descripcion,
+                                     e.marca_id,
+                                     m.nombre_marca
+                                 }).ToList();
             if (listadoEquipo.Count == 0)
             {
                 return NotFound();
             }
-
             return Ok(listadoEquipo);
         }
 
         [HttpGet]
-        [Route("GetById/{id}")]
-
-        public IActionResult Get(int id) 
-        { 
-            
+        [Route("getbyid/{id}")]
+        public IActionResult GetById(int id)
+        {
             equipos? equipo = (from e in _equiposContexto.equipos
                                where e.id_equipos == id
                                select e).FirstOrDefault();
+            if (equipo == null) return NotFound();
 
-            if (equipo == null)
-            {
-                return NotFound();
-            }
             return Ok(equipo);
-        
         }
-
         [HttpGet]
-        [Route("Find/{filtro}")]
-
-        public IActionResult FindByDescription(string filtro) 
+        [Route("find/{filtro}")]
+        public IActionResult GetByName(string filtro)
         {
-        
-            equipos? equipo = (from e in _equiposContexto.equipos
-                               where e.descripcion.Contains(filtro)
-                               select e).FirstOrDefault();
-
-            if (equipo == null)
-            {
-                return NotFound();
-            }
+            List<equipos> equipo = (from e in _equiposContexto.equipos
+                                    where e.nombre.Contains(filtro)
+                                    select e).ToList();
+            if (equipo == null) return NotFound();
 
             return Ok(equipo);
         }
 
         [HttpPost]
         [Route("Add")]
-
-        public IActionResult GuardarEquipo([FromBody] equipos equipo) 
+        public IActionResult GuardarEquipo([FromBody] equipos equipo)
         {
-
             try
             {
+
                 _equiposContexto.equipos.Add(equipo);
                 _equiposContexto.SaveChanges();
                 return Ok(equipo);
+
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
+
         }
 
         [HttpPut]
         [Route("actualizar/{id}")]
-
-        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        public IActionResult actualizarequipo(int id, [FromBody] equipos equipoModificar)
         {
-            equipos? equipoActual = (from e in _equiposContexto.equipos
-                                     where e.id_equipos == id
-                                     select e).FirstOrDefault();
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipo == null) return NotFound();
 
-            if (equipoActual == null)
-            {
-                return NotFound();
-            }
+            equipo.nombre = equipoModificar.nombre;
+            equipo.descripcion = equipoModificar.descripcion;
+            equipo.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipo.marca_id = equipoModificar.marca_id;
+            equipo.modelo = equipoModificar.modelo;
+            equipo.anio_compra = equipoModificar.anio_compra;
+            equipo.costo = equipoModificar.costo;
+            equipo.estado = equipoModificar.estado;
 
-            equipoActual.nombre=equipoModificar.nombre;
-            equipoActual.descripcion = equipoModificar.descripcion;
-            equipoActual.marca_id = equipoModificar.marca_id;
-            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
-            equipoActual.anio_compra = equipoModificar.anio_compra;
-            equipoActual.costo = equipoModificar.costo;
-
-            _equiposContexto.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContexto.Entry(equipo).State = EntityState.Modified;
             _equiposContexto.SaveChanges();
 
             return Ok(equipoModificar);
 
+
+
+
         }
-
-        [HttpDelete]
+        [HttpPut]
         [Route("eliminar/{id}")]
-
-        public IActionResult EliminarEquipo(int id)
+        public IActionResult Eliminarequipos(int id)
         {
-            equipos? equipo = (from e in _equiposContexto.equipos
-                               where e.id_equipos ==id
-                               select e).FirstOrDefault();
+            equipos? carre = (from e in _equiposContexto.equipos
+                              where e.id_equipos == id
+                              select e).FirstOrDefault();
+            if (carre == null) return NotFound();
 
-            if(equipo==null) { return NotFound(); }
-
-            _equiposContexto.equipos.Attach(equipo);
-            _equiposContexto.equipos.Remove(equipo);
+            carre.estado = "I";
+            _equiposContexto.Entry(carre).State = EntityState.Modified;
             _equiposContexto.SaveChanges();
 
-            return Ok(equipo);
+            return Ok(carre);
+
         }
 
+
+
     }
+
 }
